3d763bba57644321aafdf3bd7cd59c2d98262721
pajmd <philippejmd@gmail.com>
2020-04-18 12:51:20 +0200

    Added write_log logging function

--- wp-content/themes/wptp/functions.php
+++ wp-content/themes/wptp/functions.php
@@ -26,3 +26,18 @@ function wptp_add_menu()
 {
     register_nav_menu('WpTP Main Menu ', 'Location of wptpmenu');
 }
+
+if (!function_exists('write_log')) {
+
+    // To log data or objects
+    function write_log($log) {
+        if (true === WP_DEBUG) {
+            if (is_array($log) || is_object($log)) {
+                error_log(print_r($log, true));
+            } else {
+                error_log($log);
+            }
+        }
+    }
+}
+
